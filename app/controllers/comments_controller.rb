a1b0de401718892d714d8c964028eef6ba8ec370
Jorge Gomez <86806874+Gomezzzer@users.noreply.github.com>
2024-03-01 19:03:13 +0000

    authorization update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[ show edit update destroy ]
-  before_action :is_an_authorized_user, only: [:destroy, :create]
+  before_action :is_an_authorized_user, only: [:create]
 
   # GET /comments or /comments.json
   def index
@@ -64,6 +64,13 @@ class CommentsController < ApplicationController
       @comment = Comment.find(params[:id])
     end
 
+     # Only allow a list of trusted parameters through.
+     def comment_params
+      params.require(:comment).permit(:author_id, :photo_id, :body)
+    end
+
+    
+
     def is_an_authorized_user
       @photo = Photo.find(params.fetch(:comment).fetch(:photo_id))
       if current_user != @photo.owner && @photo.owner.private? && !current_user.leaders.include?(@photo.owner)
@@ -71,8 +78,5 @@ class CommentsController < ApplicationController
       end
     end
 
-    # Only allow a list of trusted parameters through.
-    def comment_params
-      params.require(:comment).permit(:author_id, :photo_id, :body)
-    end
+   
 end
